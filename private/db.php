5eabfb51d5806611270cfe8bcae2597eb8b88c85
Luigix456 <ditieriluis@gmail.com>
2024-11-01 17:59:44 -0300

    Modificación db

--- private/db.php
+++ private/db.php
@@ -1,9 +1,17 @@
 <?php
-$host = 'tu_host';
-$db = 'tu_base_de_datos';
-$user = 'tu_usuario';
-$pass = 'tu_contraseña';
+// Incluir autoload de Composer si es necesario
+require '../vendor/autoload.php';
+
+// Obtener las variables de entorno
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+
+// Conectar a la base de datos
 $conn = mysqli_connect($host, $user, $pass, $db);
+
+// Verificar la conexión
 if (!$conn) {
     die("Conexión fallida: " . mysqli_connect_error());
 }
